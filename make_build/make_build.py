51a313a4ccf5207a9442d0d0c10f4a64740a0009
Den <komarden22@gmail.com>
2021-05-02 02:05:41 +0300

    updated git ignore and build folder

---
+++ make_build/make_build.py
@@ -0,0 +1,18 @@
+
+
+import subprocess
+import sys
+import os
+
+sys.path.append('..')
+
+MAIN_CONST = "code/autoclicker.py"
+ICON_WINDOWS = ""
+PROGRAM_NAME = "autoclicker"
+
+line = "pyinstaller -F --distpath build --workpath build -n " + PROGRAM_NAME + " " + ICON_WINDOWS + " " + MAIN_CONST
+print(line)
+subprocess.call(line, shell=True)
+# os.remove(PROGRAM_NAME + ".spec")
+
+a = input("Press Enter to exit")
