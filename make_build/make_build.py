7ebddc01d3f88131dc73fb5323668d6c230bb873
Den <komarden22@gmail.com>
2021-05-02 02:26:44 +0300

    fix(build): fixed path

--- make_build/make_build.py
+++ make_build/make_build.py
@@ -6,11 +6,11 @@ import os
 
 sys.path.append('..')
 
-MAIN_CONST = "code/autoclicker.py"
+MAIN_CONST = "../code/autoclicker.py"
 ICON_WINDOWS = ""
 PROGRAM_NAME = "autoclicker"
 
-line = "pyinstaller -F --distpath build --workpath build -n " + PROGRAM_NAME + " " + ICON_WINDOWS + " " + MAIN_CONST
+line = "pyinstaller -F --distpath ../build --workpath ../build -n " + PROGRAM_NAME + " " + ICON_WINDOWS + " " + MAIN_CONST
 print(line)
 subprocess.call(line, shell=True)
 # os.remove(PROGRAM_NAME + ".spec")
